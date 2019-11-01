811aa4b1a679bc40501473f6224a8d92b006237a
lukaszchoros <lukasz.choros@jpsfinancial.co.uk>
2019-11-01 17:25:48 +0000

    camel back

---
+++ Camel/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Camel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            int m = int.Parse(Console.ReadLine());
+
+            int round = 0;
+            if (numbers.Count == m)
+            {
+                Console.WriteLine("already stable: {0}", string.Join(" ", numbers));
+            }
+            else
+            {
+                while(numbers.Count != m)
+                {
+                    numbers.RemoveAt(0);
+                    numbers.RemoveAt(numbers.Count-1);
+                    round++;
+                }
+                Console.WriteLine("{0} rounds", round);
+                Console.WriteLine("remaining: {0}", string.Join(" ", numbers));
+            }
+        }
+    }
+}
